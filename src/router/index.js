cacf4d10953a2759d620d6660c83dc654121f931
alexoa <alex00yt@gmail.com>
2023-12-10 18:33:39 -0600

    vue router fix issue when navigating dif routes: debug

--- src/router/index.js
+++ src/router/index.js
@@ -116,6 +116,7 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
+  console.log(import.meta.env.BASE_URL);
   const userStore = useUserStore();
   const generalVariablesStore = useGeneralVariablesStore();
   if (to.name == "home") generalVariablesStore.homePageNavbarColor = true;
